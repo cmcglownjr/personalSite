9a05052391d427d14613a9bc02f9c547030e35de
Chris McGlown <cmcglown@tutamail.com>
2023-03-09 21:11:06 -0500

    added project and testimonial models. made too many spelling errors.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Testimonial, Project
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['reference_name', 'reference_title', 'reference_testimonial', 'image_url', 'created']
+    list_filter = ['reference_name', 'created']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'repo', 'demo', 'screenshot', 'project_created', 'created']
+    list_filter = ['name', 'project_created']
--- base/models.py
+++ base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    reference_name = models.CharField(max_length=150)
+    reference_title = models.CharField(max_length=200)
+    reference_testimonial = models.TextField()
+    reference_url = models.URLField(blank=True)
+    image_url = models.URLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.reference_name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    repo = models.URLField()
+    demo = models.URLField(blank=True)
+    screenshot = models.ImageField()
+    project_created = models.DateTimeField(db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
