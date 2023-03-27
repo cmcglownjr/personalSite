c328da1425cc5edc1889d02aab92b39c2f7a9831
Chris McGlown <cmcglown@tutamail.com>
2023-03-27 13:18:15 -0400

    Fixed issue with admin site not showing css styles. Removed static files from git cache. Added static files folder to gitignore.

--- personalSite/settings.py
+++ personalSite/settings.py
@@ -140,11 +140,7 @@ STORAGES = {
 }
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
