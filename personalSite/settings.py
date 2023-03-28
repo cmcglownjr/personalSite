0cb670716f55133803add50b6b5cb6eb78f8086f
Chris McGlown <cmcglown@tutamail.com>
2023-03-27 22:27:04 -0400

    Added job titles to experience section. Changed color scheme in experience section.

--- personalSite/settings.py
+++ personalSite/settings.py
@@ -140,7 +140,7 @@ STORAGES = {
 }
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
