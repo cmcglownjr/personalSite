ad5846e4a50984648a07a53aae0258239db95c71
Chris McGlown <eezyville@protonmail.com>
2023-05-02 21:30:35 -0400

    Starting to build failing tests.

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse("base:home"))
+        self.assertEquals(response.status_code, 200)
